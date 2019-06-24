cc009c90c9265961e9ba4efca2d2ed80337707eb
Soumitra Roy Sarkar <projwebinfo@gmail.com>
2019-06-24 16:30:33 +0530

    Add files via upload

---
+++ php-os-detection/php-os-detection.php
@@ -0,0 +1,35 @@
+<?php
+
+function get_operating_system() {
+    $u_agent = $_SERVER['HTTP_USER_AGENT'];
+    $operating_system = 'Unknown Operating System';
+
+    //Get the operating_system
+    if (preg_match('/linux/i', $u_agent)) {
+        $operating_system = 'Linux';
+    } elseif (preg_match('/macintosh|mac os x|mac_powerpc/i', $u_agent)) {
+        $operating_system = 'Mac';
+    } elseif (preg_match('/windows|win32|win98|win95|win16/i', $u_agent)) {
+        $operating_system = 'Windows';
+    } elseif (preg_match('/ubuntu/i', $u_agent)) {
+        $operating_system = 'Ubuntu';
+    } elseif (preg_match('/iphone/i', $u_agent)) {
+        $operating_system = 'IPhone';
+    } elseif (preg_match('/ipod/i', $u_agent)) {
+        $operating_system = 'IPod';
+    } elseif (preg_match('/ipad/i', $u_agent)) {
+        $operating_system = 'IPad';
+    } elseif (preg_match('/android/i', $u_agent)) {
+        $operating_system = 'Android';
+    } elseif (preg_match('/blackberry/i', $u_agent)) {
+        $operating_system = 'Blackberry';
+    } elseif (preg_match('/webos/i', $u_agent)) {
+        $operating_system = 'Mobile';
+    }
+    
+    return $operating_system;
+}
+
+echo get_operating_system();
+
+?>
