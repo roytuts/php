05a1f43ceb570c122e8b72187b236cc9d68cf82f
Soumitra <projwebinfo@gmail.com>
2022-06-09 11:41:43 +0530

    Update php-os-detection.php

--- php-os-detection/php-os-detection.php
+++ php-os-detection/php-os-detection.php
@@ -1,31 +1,35 @@
 <?php
 
 function get_operating_system() {
-    $u_agent = $_SERVER['HTTP_USER_AGENT'];
+    $u_agent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
     $operating_system = 'Unknown Operating System';
 
     //Get the operating_system name
-    if (preg_match('/linux/i', $u_agent)) {
-        $operating_system = 'Linux';
-    } elseif (preg_match('/macintosh|mac os x|mac_powerpc/i', $u_agent)) {
-        $operating_system = 'Mac';
-    } elseif (preg_match('/windows|win32|win98|win95|win16/i', $u_agent)) {
-        $operating_system = 'Windows';
-    } elseif (preg_match('/ubuntu/i', $u_agent)) {
-        $operating_system = 'Ubuntu';
-    } elseif (preg_match('/iphone/i', $u_agent)) {
-        $operating_system = 'IPhone';
-    } elseif (preg_match('/ipod/i', $u_agent)) {
-        $operating_system = 'IPod';
-    } elseif (preg_match('/ipad/i', $u_agent)) {
-        $operating_system = 'IPad';
-    } elseif (preg_match('/android/i', $u_agent)) {
-        $operating_system = 'Android';
-    } elseif (preg_match('/blackberry/i', $u_agent)) {
-        $operating_system = 'Blackberry';
-    } elseif (preg_match('/webos/i', $u_agent)) {
-        $operating_system = 'Mobile';
-    }
+	if($u_agent) {
+		if (preg_match('/linux/i', $u_agent)) {
+			$operating_system = 'Linux';
+		} elseif (preg_match('/macintosh|mac os x|mac_powerpc/i', $u_agent)) {
+			$operating_system = 'Mac';
+		} elseif (preg_match('/windows|win32|win98|win95|win16/i', $u_agent)) {
+			$operating_system = 'Windows';
+		} elseif (preg_match('/ubuntu/i', $u_agent)) {
+			$operating_system = 'Ubuntu';
+		} elseif (preg_match('/iphone/i', $u_agent)) {
+			$operating_system = 'IPhone';
+		} elseif (preg_match('/ipod/i', $u_agent)) {
+			$operating_system = 'IPod';
+		} elseif (preg_match('/ipad/i', $u_agent)) {
+			$operating_system = 'IPad';
+		} elseif (preg_match('/android/i', $u_agent)) {
+			$operating_system = 'Android';
+		} elseif (preg_match('/blackberry/i', $u_agent)) {
+			$operating_system = 'Blackberry';
+		} elseif (preg_match('/webos/i', $u_agent)) {
+			$operating_system = 'Mobile';
+		}
+	} else {
+		$operating_system = php_uname('s');
+	}
     
     return $operating_system;
 }
