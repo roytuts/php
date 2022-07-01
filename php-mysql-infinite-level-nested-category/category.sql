31c4c237b6e4e6ea91d0cd8e9535cf342b8c0472
Soumitra <projwebinfo@gmail.com>
2022-07-01 21:25:23 +0530

    Update category.sql

--- php-mysql-infinite-level-nested-category/category.sql
+++ php-mysql-infinite-level-nested-category/category.sql
@@ -1,34 +1,13 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               8.0.17 - MySQL Community Server - GPL
--- Server OS:                    Win64
--- HeidiSQL Version:             10.2.0.5599
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
-
--- Dumping database structure for roytuts
-CREATE DATABASE IF NOT EXISTS `roytuts` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `roytuts`;
-
--- Dumping structure for table roytuts.category
 CREATE TABLE IF NOT EXISTS `category` (
-  `category_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `category_id` int UNSIGNED COLLATE utf8mb4_unicode_ci NOT NULL AUTO_INCREMENT,
   `category_name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `category_link` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `parent_id` int(10) unsigned NOT NULL DEFAULT '0',
-  `sort_order` int(11) NOT NULL DEFAULT '0',
+  `parent_id` int UNSIGNED  COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '0',
+  `sort_order` int COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '0',
   PRIMARY KEY (`category_id`),
   UNIQUE KEY `unique` (`category_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=UTF8MB4_UNICODE_CI;
 
--- Dumping data for table roytuts.category: ~14 rows (approximately)
-/*!40000 ALTER TABLE `category` DISABLE KEYS */;
 INSERT INTO `category` (`category_id`, `category_name`, `category_link`, `parent_id`, `sort_order`) VALUES
 	(1, 'Home', 'home', 0, 0),
 	(2, 'Tutorials', 'tutorials', 0, 1),
@@ -44,8 +23,12 @@ INSERT INTO `category` (`category_id`, `category_name`, `category_link`, `parent
 	(12, 'SOAP', 'soap', 10, 3),
 	(13, 'Contact', 'contact', 0, 4),
 	(14, 'About', 'about', 0, 5);
-/*!40000 ALTER TABLE `category` ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+	
+SELECT
+  `category_id`,
+  `category_name`,
+  `category_link`,
+  `parent_id`,
+  `sort_order`
+FROM `roytuts`.`category`
+ORDER BY parent_id, sort_order, category_name;
